8235954f22b8f97c1828dc24746883045dd359e4
Daniel Nascimento <dpericles6@gmail.com>
2018-11-23 11:22:05 -0200

    vue_start

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+                <hr>
+             <div class="row">
+                <app-service></app-service>
+                <app-details></app-details>
+            </div>
+                <hr>
+                <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import Service from './Service.vue';
+    import Details from './Details.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-service': Service,
+            'app-details': Details
+        }
+    }
+</script>
+
+<style>
+
+</style>
